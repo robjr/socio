8771020969839fd0d28ff1013620640561125b68
Roberto Junior <junior.hardware.jesus@gmail.com>
2016-09-01 02:25:12 -0300

    Cria classe para validacao de tipos

---
+++ application/helpers/typeexception_helper.php
@@ -0,0 +1,14 @@
+<?php
+
+class TypeException extends Exception {
+
+    const STRING = "TIPO invalido! O tipo permitido eh STRING.";
+    const INTEGER = "TIPO invalido! O tipo permitido eh INTEGER.";
+    const DOUBLE = "TIPO invalido! O tipo permitido eh DOUBLE.";
+    const FLOAT = "TIPO invalido! O tipo permitido eh FLOAT.";
+    const BOOLEAN = "TIPO invalido! O tipo permitido eh BOOLEAN.";
+    const CPF = "CPF inválido.";
+    const CNPJ = "CNPJ inválido.";
+    const EMAIL = "EMAIL inválido.";
+
+}
